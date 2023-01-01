05ee7630853395d67f36eea1340210a5c2448415
EC2 Default User <ec2-user@ip-172-31-32-189.ap-northeast-1.compute.internal>
2023-01-01 11:08:21 +0000

    app2-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramenters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_paramenters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 end
