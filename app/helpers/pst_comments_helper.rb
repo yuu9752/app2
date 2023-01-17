7f7cbb4ed0c8520f8e26bdff8655cf98aaaa87a2
EC2 Default User <ec2-user@ip-172-31-32-189.ap-northeast-1.compute.internal>
2023-01-17 18:02:57 +0000

    app2-19

---
+++ app/helpers/pst_comments_helper.rb
@@ -0,0 +1,2 @@
+module PstCommentsHelper
+end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,9 @@ class PostImage < ApplicationRecord
   
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
   
   def get_image
     if image.attached?
@@ -17,6 +20,10 @@ class PostImage < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'defaulr-image.jpg', content_type: 'image/jpeg')
     end
     image
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
   
 end
